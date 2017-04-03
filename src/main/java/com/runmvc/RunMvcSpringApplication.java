2827dadcb7d1ca6b988f346d653b7ddef657905b
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-03 14:01:32 -0500

    Start project

---
+++ src/main/java/com/runmvc/RunMvcSpringApplication.java
@@ -0,0 +1,12 @@
+package com.runmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunMvcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunMvcSpringApplication.class, args);
+	}
+}
